9c7f31fec884581dec9e33e1816ab55abd4a9d72
amarjapatil4 <96417391+amarjapatil4@users.noreply.github.com>
2023-01-04 14:40:47 +0530

    Generating Error

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,3 +1,30 @@
 def call(String name = 'User') {
-		echo "Welcome, ${name}."
+	echo "Welcome, ${name}."
+	pipeline {
+	    agent any
+	    stages {
+		stage("build") {
+		    steps {
+			echo 'Hello1 '
+			error 'Failed step1'
+		    }
+		}
+		stage("test") {
+		    steps {
+			echo 'Hello2 '
+		    }
+		}
+		stage("deploy") {
+		    steps {
+			echo 'Hello3 '
+			error 'Failed step2'
+		    }
+		}
+		stage("done") {
+		    steps {
+			echo 'Hello3 '
+		    }
+		}
+	    }
+	}
 }
