62e05c4522572259b40d8e163fc8d0109dbcb840
amarjapatil4 <96417391+amarjapatil4@users.noreply.github.com>
2023-01-12 22:04:51 +0530

    Update test.js

--- vars/test.js
+++ vars/test.js
@@ -1,3 +1,15 @@
 window.addEventListener("load", function() {
+    let headers = new Headers();
+    let fetchRes = fetch(
+    "https://jsonplaceholder.typicode.com/todos/1", {mode: 'cors',
+    credentials: 'omit',
+    headers: headers});
+
+    // fetchRes is the promise to resolve
+    // it by using.then() method
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+        })
     document.querySelector("#main-panel").style.backgroundColor = "red";
 })
